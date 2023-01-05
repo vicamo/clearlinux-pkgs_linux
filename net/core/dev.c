ea1c24fca679b85381944f3e5ded064094102d91
Arjan van de Ven <arjan.van.de.ven@intel.com>
2023-01-05 16:52:33 +0000

    add networking support for powerbump
    Signed-off-by: Continuous Integration <username@users.noreply.github.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -150,6 +150,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/prandom.h>
 #include <linux/once_lite.h>
+#include <linux/powerbump.h>
 
 #include "dev.h"
 #include "net-sysfs.h"
@@ -5739,6 +5740,7 @@ int netif_receive_skb(struct sk_buff *skb)
 	int ret;
 
 	trace_netif_receive_skb_entry(skb);
+	give_power_bump(BUMP_FOR_NETWORK);
 
 	ret = netif_receive_skb_internal(skb);
 	trace_netif_receive_skb_exit(ret);
@@ -5763,6 +5765,7 @@ void netif_receive_skb_list(struct list_head *head)
 
 	if (list_empty(head))
 		return;
+	give_power_bump(BUMP_FOR_NETWORK);
 	if (trace_netif_receive_skb_list_entry_enabled()) {
 		list_for_each_entry(skb, head, list)
 			trace_netif_receive_skb_list_entry(skb);
